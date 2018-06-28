227e9003758093071f5f66a8d465ca7ee256c897
Rafalkufel <rafal.kufel@gmail.com>
2018-06-28 12:59:25 +0200

    added frame with staff notebook with studentlist and staffmeeting list

---
+++ frontend/frames/staffframe.py
@@ -0,0 +1,107 @@
+from tkinter import Button, Entry, Label, LabelFrame, Listbox, DISABLED, ACTIVE
+from tkinter.ttk import Entry, Label, Labelframe, Treeview
+
+
+class StaffFrame(Labelframe):
+    """Contains data of staffmeeteng"""
+    def __init__(self, window, base, **kwargs):
+        Labelframe.__init__(self, window, **kwargs)
+        self.base = base
+        self.staff_id = None
+        self.all_staff = [x[0] for x in self.base.get_all_staff()]
+        self.data_label = Label(self, text="Data zespołu")
+        self.data_label.grid(row=0, column=0, padx=5, pady=10, sticky='e')
+        self.data_entry = Entry(self, width=20)
+        self.data_entry.grid(row=0, column=1, padx=5, pady=5, sticky='w')
+
+        self.table = Treeview(self, columns=("name", "speciality"))
+        self.table.heading('#1', text='imię i nazwisko')
+        self.table.heading('#2', text='specjalizacja')
+        self.table.column('#1', width = 200)
+        self.table.column('#2', width = 200)
+
+        self.table.grid(row=1,column=0, rowspan=3, columnspan=2, padx=5, pady=5)
+        self.table['show'] = 'headings'
+        self.table.bind('<<TreeviewSelect>>', self.on_treeview_selected)
+
+        self.another_stuff_frame = Labelframe(self, text="Inni specjaliści")
+        self.another_stuff_frame.grid(row=0, column=2, rowspan=2, pady=5)
+        
+        self.another_staff = Listbox(self.another_stuff_frame)
+        self.another_staff.grid(row=0, column=2, rowspan=2)
+        self.another_staff.bind('<<ListboxSelect>>', self.on_listbox_select)
+        self.add_button = Button(self, text="Dodaj członka", command=self.add_member)
+        self.add_button.grid(row=2, column=2)
+        self.delete_button = Button(self, text="Usuń członka", command=self.remove_member)
+        self.delete_button.grid(row=3, column=2)
+    
+    def get_all_staff(self):
+        return [x[0] for x in self.base.get_all_staff()]
+    
+    def get_staff_from_table(self):
+        '''return list of member of staff_meeting from table
+        eg. [[name, speciality],[name, speciality]]'''
+        staff_meeting_list = []
+        for child in self.table.get_children():
+            staff_meeting_list.append(
+                self.table.item(child)["values"]
+                )
+        return staff_meeting_list
+    
+    def insert_staff(self, base, staff):
+        self.base = base
+        self.staff=staff
+        self.staff_id = staff['id']
+        if not self.staff_id:
+            self.tables_tidying()
+            return
+        self.data_entry.delete(0, 'end')
+        self.data_entry.insert(0, staff['date'])
+        self.table.delete(*self.table.get_children())
+        self.another_staff.delete(0, 'end')
+        for i in staff['team']:
+            self.table.insert('', 'end', values=(i[0], i[1]))
+        self.tables_tidying()
+
+    def on_listbox_select(self, event):
+        if not isinstance(self.table.selection(), str):
+            self.table.selection_remove(self.table.selection()[0])
+
+    def on_treeview_selected(self, event):
+        self.another_staff.select_clear(0, 'end')
+    
+    def tables_tidying(self):
+        self.another_staff.delete(0, 'end')
+        used_staff = [x[0] for x in self.get_staff_from_table()]
+        for member in self.get_all_staff():
+            if member not in used_staff:
+                self.another_staff.insert('end', member)
+
+    def add_member(self):
+        if not self.another_staff.curselection():
+            pass
+        else:
+            self.table.insert(
+            '',
+            'end',
+            values=(
+                self.another_staff.selection_get(),
+                self.base.what_speciality(
+                    self.another_staff.selection_get()
+                    )[1]
+                )
+            )
+
+            self.tables_tidying()
+        
+    def remove_member(self):
+        selected_item = self.table.selection()
+        if selected_item:
+            self.table.delete(selected_item)
+        self.tables_tidying()
+
+    def clear(self):
+        self.table.delete(*self.table.get_children())
+        self.tables_tidying()
+
+
---
+++ frontend/frames/studentdata.py
@@ -0,0 +1,101 @@
+from tkinter import  Button, Frame, LabelFrame, Label, Listbox
+from tkinter.ttk import Button, Frame, Labelframe, Label, Notebook, Treeview
+import collections
+
+
+class ListOfData(Labelframe):
+    """Contains list of student in database"""
+    def __init__(self, window, base, **kwargs):
+        Labelframe.__init__(self, window, **kwargs)
+        self.base = base
+        self.notebook = Notebook(self)
+        self.notebook.grid(row=0, column=0, padx=5, pady=5)
+
+        self.student = Frame(self.notebook)
+        self.student.grid(row=0, column=0)
+        self.student_list = Treeview(self.student, height=29, columns=("name", "pesel"), displaycolumns="name")
+        self.student_list.heading('name', text="Nazwisko i imię", anchor='w')
+        self.student_list.column('name', width=240)
+        self.student_list['show'] = 'headings'
+        self.student_list.grid(row=0, column=0)
+
+        self.button = Button(self.student, text="Usuń")
+        self.button.grid(row=1, column=0)
+
+        self.staff_meeting = Frame(self.notebook)
+        self.staff_meeting.grid(row=0, column=0)
+        self.table = Treeview(self.staff_meeting, height=29, columns=("name", "id", "student_id"), displaycolumns="name")
+        self.table.heading('name', text='Zespoły', anchor='w')
+        self.table.column('name', width=235)
+        self.table.column('#0', stretch=0, width=10)
+        self.table.grid(row=0, column=0)
+        self.delete_staffmeeting= Button(self.staff_meeting, text="Usuń wpis", command=self.delete_from_staffmeeting)
+        self.delete_staffmeeting.grid(row=1, column=0)
+
+        self.notebook.add(self.student, text = "Dzieci")
+        self.notebook.add(self.staff_meeting, text = "Zespoły")
+
+    def get_students(self):
+        return self.base.get_dict_of_students()
+
+    def get_pesel_from_name(self, name):
+        students = self.get_students()
+        for pesel in students:
+            if students[pesel] == name:
+                return pesel
+            
+    def fill_student_list(self):
+        self.student_list.delete(*self.student_list.get_children())
+        if not self.get_students():
+            return
+        
+        for student, pesel in zip(
+            self.get_students().values(),
+            self.get_students().keys()
+            ):
+            self.student_list.insert("", 'end', values=(student, pesel))
+
+    def delete_from_student_list(self):
+        selected_item = self.student_list.selection()
+        pesel = self.student_list.item(selected_item)['values'][1]
+        self.base.delete_student(pesel)
+        self.fill_student_list()
+
+    def fill_staffmeeting_list(self):
+        self.table.delete(*self.table.get_children())
+        staff_meeting = self.base.staff_meeting_list()
+        meeting_in_table = {} # {value: iid}
+        for i in staff_meeting:
+            if i[1] not in meeting_in_table.keys():
+                meeting_in_table[i[1]] = self.table.insert(
+                    "",
+                    'end',
+                    values=(i[1],)
+                    )
+                actual_iid = self.table.insert(
+                    meeting_in_table[i[1]],
+                    'end',
+                    values=(self.base.get_student_name(i[20]),)
+                    )
+                self.table.set(actual_iid, column="id", value=i[0])
+                self.table.set(actual_iid, column="student_id", value=i[20])
+            else:
+                actual_iid = self.table.insert(
+                    meeting_in_table[i[1]],
+                    'end',
+                    values=(self.base.get_student_name(i[20]),)
+                    )
+                self.table.set(actual_iid, column="id", value=i[0])
+                self.table.set(actual_iid, column="student_id", value=i[20])
+        for idd in meeting_in_table.values():
+            if not self.table.parent(idd):
+                self.table.item(idd, open=True)
+            
+    def delete_from_staffmeeting(self):
+        selected_item = self.table.selection()
+        staffmeeting_id = self.table.item(selected_item)['values'][1]
+        self.base.delete_from_staffmeeting(staffmeeting_id)
+        self.fill_staffmeeting_list()
+
+
+
