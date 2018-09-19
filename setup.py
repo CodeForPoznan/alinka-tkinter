8fa6bdeb8770ff2d8295a62bf233bfb6d4a7759f
Rafalkufel <rafal.kufel@gmail.com>
2018-09-19 18:28:24 +0200

    setup for cx_freeze

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+from cx_Freeze import setup, Executable
+
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+base = None
+print(sys.platform)
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+options = {
+    'build_exe': {
+        'include_files': [
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll')
+            ]
+        }
+    }
+
+setup(
+        name = "Alinka",
+        version = "0.1",
+        options = options,
+        description = "Program wspomagajacy wydawanie orzeczen",
+        executables = [Executable("main.py", base = base)]
+        )
