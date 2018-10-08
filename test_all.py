9162c51982482b18b6472a33729233664d056f46
Rafał Kufel <rafal.kufel@gmail.com>
2018-10-08 13:51:42 +0200

    coverage as pytest plugin (fixes #16) (#44)

--- test_all.py
+++ test_all.py
@@ -7,15 +7,14 @@ class testTest(unittest.TestCase):
     def setUp(self):
         self.instance = MainWindow(Tk())
 
-    def test(self):
-        '''Test if mainwindow and frames contains a name'''
+    def test_mainwindow_and_frames_contains_a_name(self):
         self.assertEqual(self.instance.notebook['text'], "Baza danych")
         self.assertEqual(self.instance.actual_student['text'], "Dane dziecka")
         self.assertEqual(
             self.instance.applicationframe['text'],
             "Wnioskodawcy"
-            )
+        )
         self.assertEqual(
             self.instance.staff_meeting_frame['text'],
             "Zespół orzekający"
-            )
+        )
